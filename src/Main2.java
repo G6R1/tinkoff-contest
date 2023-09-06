7f55457e7065031265d4d016124853af95e6fdb6
unknown <Gerian109@yandex.ru>
2023-09-06 18:13:22 +0300

    need refact statescounter

--- src/Main2.java
+++ src/Main2.java
@@ -8,8 +8,8 @@ public class Main2 {
         int citiesNum = scanner.nextInt();
         int roadsNum = scanner.nextInt();
         Map<Integer, List<Route>> routes = new HashMap<>();
-        int minLenght = 1000000000;
-        int maxLenght = 0;
+        int minLength = 1000000000;
+        int maxLength = 0;
 
         for (int i = 0; i < roadsNum; i++) {
             int from = scanner.nextInt();
@@ -21,35 +21,42 @@ public class Main2 {
             int length = scanner.nextInt();
             routes.get(from).add(new Route(to, length));
 
-            if (length > maxLenght)
-                maxLenght = length;
-            if (length < minLenght)
-                minLenght = length;
+            if (length > maxLength)
+                maxLength = length;
+            if (length < minLength)
+                minLength = length;
         }
 
         int oldStates = statesCount(citiesNum, routes);
 
         while (true) {
-            if (maxLenght - 1 == minLenght) {
-                System.out.println(maxLenght);
+            if (maxLength == minLength) {
+                System.out.println(minLength);
                 return;
             }
 
-            int check = (maxLenght + minLenght) / 2;
-            Map<Integer, List<Route>> routesClone = new HashMap<Integer,List<Route>>(routes);
-            routesClone.forEach((key, value) -> value.removeIf(route -> route.getLength() <= check)); //некорректно удаляет маршруты
+            int check = minLength + (maxLength - minLength) / 2 + 1;
+            Map<Integer, List<Route>> routesClone = new HashMap<>();
+            for (Map.Entry<Integer, List<Route>> entry : routes.entrySet()) {
+                routesClone.put(entry.getKey(), new ArrayList<>(entry.getValue()));
+            }
+
+            routesClone.forEach((key, value) -> value.removeIf(route -> {
+                System.out.println(route.getLength() + " <= " + check + " = " + (route.getLength() <= check));
+                return route.getLength() <= check;
+            }));
             int newStates = statesCount(citiesNum, routesClone);
             if (newStates > oldStates) {
-                maxLenght -= maxLenght - check;
+                maxLength = check - 1;
             } else {
-                minLenght += check - minLenght;
+                minLength = check;
             }
         }
     }
 
     static class Route {
-        private int to;
-        private int length;
+        private final int to;
+        private final int length;
 
         public Route(int to, int length) {
             this.to = to;
@@ -60,39 +67,29 @@ public class Main2 {
             return to;
         }
 
-        public void setTo(int to) {
-            this.to = to;
-        }
-
         public int getLength() {
             return length;
-        }
-
-        public void setLength(int length) {
-            this.length = length;
         }
     }
 
     static int statesCount(int citiesNum, Map<Integer, List<Route>> map) { //тут неверный подсчет
         int count = 0;
-        Set<Integer> citiesIndices = IntStream.rangeClosed(1 , citiesNum).boxed().collect(Collectors.toSet());
+        Set<Integer> citiesIndices = IntStream.rangeClosed(1, citiesNum).boxed().collect(Collectors.toSet());
         Queue<Integer> queue = new ArrayDeque<>();
 
         while (!citiesIndices.isEmpty()) {
-            queue.add(citiesIndices.iterator().next());
+            queue.add(citiesIndices.iterator().next()); //что если первый элеемент является конечной точкой. от него не идет пути, но идет к нему
             citiesIndices.remove(queue.peek());
             while (!queue.isEmpty()) {
-                if (map.get(queue.peek()) == null){
-                    queue.poll();
-                    continue;
+                List<Route> list = map.get(queue.poll());
+                if (list != null) {
+                    list.forEach(x -> {
+                        if (citiesIndices.contains(x.getTo())) {
+                            queue.add(x.getTo());
+                        }
+                        citiesIndices.remove(x.getTo());
+                    });
                 }
-                map.get(queue.poll())
-                        .forEach(x -> {
-                            if (citiesIndices.contains(x.getTo())) {
-                                queue.add(x.getTo());
-                            }
-                            citiesIndices.remove(x.getTo());
-                        });
             }
             count++;
         }
@@ -100,13 +97,18 @@ public class Main2 {
     }
 }
 /*
-8 8
-1 2 10
-2 3 15
+8 9
+1 2 15
+2 3 10
 3 4 20
 4 1 25
+4 3 60
 5 6 40
 6 6 20
 7 8 10
 8 7 50
+
+2 2
+1 2 20
+2 1 5
  */
